d1a26f3e8ad809029b67454935c757dc0e6c7e80
jcbordon96 <jcbordon96@gmail.com>
2021-10-28 13:55:49 -0300

    Agregue archivos de test ADC y DHT

---
+++ adc_test.py
@@ -0,0 +1,51 @@
+# Simple demo of reading each analog input from the ADS1x15 and printing it to
+# the screen.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
+import math
+
+
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Or create an ADS1015 ADC (12-bit) instance.
+#adc = Adafruit_ADS1x15.ADS1015()
+
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
+
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
+# Main loop.
+while True:
+    # Read all the ADC channel values in a list.
+    values = [0]*4
+    value = adc.read_adc(0, gain=GAIN)
+    print("La medicion del ADC es de: " + str(value))
+    volt = (value/32768)*4.096
+    RS = ((3.3/volt)-1)*15
+    RO = RS/3.6
+    ro = 3.7813
+    ratio = RS/ro
+    ppm = pow((math.log(ratio,10)-0.323)/(-0.243),10)
+    print("El voltaje: {}, rs: {}, ro: {}, ppm: {}".format(volt,RS, RO,ppm))
+    
+    time.sleep(0.5)
